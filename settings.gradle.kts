14543628ba34a19cfa567d831f5db1eb44779f1f
Abraham Evan Keane Nelwan <evankeane15@gmail.com>
2025-03-21 15:00:19 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "package com.Abraham Evan Keane Nelwan_607062300068_asesment1"
+include(":app")
+ 
